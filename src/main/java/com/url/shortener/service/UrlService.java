3b7abe12c1fb5dd9881540e9249b5faa004ebeda
kylekim2123 <kyle2123@naver.com>
2023-09-27 23:09:50 +0900

    feat: Short Url 생성 관련 레포지토리, 서비스 코드 작성

---
+++ src/main/java/com/url/shortener/service/UrlService.java
@@ -0,0 +1,27 @@
+package com.url.shortener.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.url.shortener.domain.Url;
+import com.url.shortener.dto.request.UrlRequest;
+import com.url.shortener.dto.response.UrlIdResponse;
+import com.url.shortener.repository.UrlRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UrlService {
+
+    private final UrlRepository urlRepository;
+
+    @Transactional
+    public UrlIdResponse createShortUrl(UrlRequest urlRequest) {
+        Url url = urlRequest.toEntity();
+        Url savedUrl = urlRepository.save(url);
+
+        return UrlIdResponse.from(savedUrl.getId());
+    }
+}
