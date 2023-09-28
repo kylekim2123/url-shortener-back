34332110b64bdf5e50b9aa885bf04ac3e99820ff
kylekim2123 <kyle2123@naver.com>
2023-09-29 02:57:15 +0900

    test: Short URL 조회 및 리다이렉트 기능에 대한 테스트 코드 추가

--- src/test/java/com/url/shortener/service/UrlServiceTest.java
+++ src/test/java/com/url/shortener/service/UrlServiceTest.java
@@ -3,23 +3,39 @@ package com.url.shortener.service;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.url.shortener.domain.Url;
 import com.url.shortener.dto.request.UrlRequest;
 import com.url.shortener.dto.response.UrlIdResponse;
+import com.url.shortener.dto.response.UrlResponse;
 import com.url.shortener.repository.UrlRepository;
 
 @SpringBootTest
 class UrlServiceTest {
+
+    private static final String INIT_ORIGINAL_URL = "https://www.urlShortener.com/";
 
     @Autowired
     private UrlService urlService;
 
     @Autowired
     private UrlRepository urlRepository;
+
+    @Value("${env.short-url-domain}")
+    private String urlDomain;
+
+    @BeforeEach
+    void initData() {
+        //given
+        UrlRequest urlRequest = UrlRequest.from(INIT_ORIGINAL_URL);
+        urlService.createShortUrl(urlRequest);
+    }
 
     @AfterEach
     void deleteAll() {
@@ -29,21 +45,55 @@ class UrlServiceTest {
     @Test
     @DisplayName("임의의 URL의 단축 정보를 생성하여 저장된 short URL의 ID를 반환한다.")
     void 단축_URL_생성_여부_테스트() {
-        UrlRequest urlRequest = UrlRequest.from("https://www.naver.com/");
+        //when
+        UrlRequest urlRequest = UrlRequest.from("https://www.newUrlShortener.com/");
         UrlIdResponse urlIdResponse = urlService.createShortUrl(urlRequest);
 
-        assertThat(urlIdResponse.getId()).isEqualTo(1L);
+        //then
+        long allUrlsCount = urlRepository.count();
+        assertThat(allUrlsCount).isEqualTo(2);
     }
 
     @Test
     @DisplayName("이미 존재하는 URL의 단축 정보를 생성할 때, 새로 생성하지 않고 기존 short URL의 ID를 반환한다.")
     void 단축_URL_중복_생성_여부_테스트() {
-        UrlRequest urlRequest1 = UrlRequest.from("https://www.naver.com/");
-        UrlIdResponse urlIdResponse1 = urlService.createShortUrl(urlRequest1);
+        //when
+        UrlRequest urlRequest = UrlRequest.from(INIT_ORIGINAL_URL);
+        UrlIdResponse urlIdResponse = urlService.createShortUrl(urlRequest);
 
-        UrlRequest urlRequest2 = UrlRequest.from("https://www.naver.com/");
-        UrlIdResponse urlIdResponse2 = urlService.createShortUrl(urlRequest2);
+        //then
+        Url existedUrl = urlRepository.findByOriginalUrl(INIT_ORIGINAL_URL).get();
+        assertThat(urlIdResponse.getId()).isEqualTo(existedUrl.getId());
+        assertThat(existedUrl.getShorteningCount()).isEqualTo(2);
+    }
 
-        assertThat(urlIdResponse2.getId()).isEqualTo(urlIdResponse1.getId());
+    @Test
+    @DisplayName("short URL의 id값을 기준으로 정상적으로 UrlResponse를 반환한다.")
+    void ID값_기준_정상_조회_테스트() {
+        //given
+        Url savedUrl = urlRepository.findByOriginalUrl(INIT_ORIGINAL_URL).get();
+        String key = savedUrl.getShortUrlKey();
+
+        //when
+        UrlResponse urlResponse = urlService.findShortUrlById(savedUrl.getId());
+
+        //then
+        assertThat(urlResponse.getId()).isEqualTo(savedUrl.getId());
+        assertThat(urlResponse.getOriginalUrl()).isEqualTo(INIT_ORIGINAL_URL);
+        assertThat(urlResponse.getShortUrl()).isEqualTo(urlDomain + key);
+    }
+
+    @Test
+    @DisplayName("short URL의 key값을 기준으로 정상적으로 Original Url을 반환한다.")
+    void KEY값_기준_정상_ORIGINAL_URL_조회_테스트() {
+        //given
+        Url savedUrl = urlRepository.findByOriginalUrl(INIT_ORIGINAL_URL).get();
+        String key = savedUrl.getShortUrlKey();
+
+        //when
+        String originalUrl = urlService.getOriginalUrlByShortUrlKey(key);
+
+        //then
+        assertThat(originalUrl).isEqualTo(INIT_ORIGINAL_URL);
     }
 }
