16fbcac2439af391ea3cc7a511f86f3666f9262e
kylekim2123 <kyle2123@naver.com>
2023-10-05 19:37:49 +0900

    refactor: Response DTO에서 불필요한 기본생성자 삭제

--- src/main/java/com/url/shortener/dto/response/UrlIdResponse.java
+++ src/main/java/com/url/shortener/dto/response/UrlIdResponse.java
@@ -1,12 +1,9 @@
 package com.url.shortener.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(staticName = "from")
 public class UrlIdResponse {
 
--- src/main/java/com/url/shortener/dto/response/UrlResponse.java
+++ src/main/java/com/url/shortener/dto/response/UrlResponse.java
@@ -4,16 +4,11 @@ import java.time.LocalDateTime;
 
 import com.url.shortener.domain.Url;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UrlResponse {
 
     private Long id;
