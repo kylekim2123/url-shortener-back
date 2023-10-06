c73aebe1d330dbac08df58ffafa033d12aed3a68
kylekim2123 <kyle2123@naver.com>
2023-10-06 17:25:17 +0900

    build: 프로젝트 프로필 분리

--- src/main/java/com/url/shortener/config/MvcConfiguration.java
+++ src/main/java/com/url/shortener/config/MvcConfiguration.java
@@ -2,11 +2,13 @@ package com.url.shortener.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile({"local", "dev"})
 public class MvcConfiguration implements WebMvcConfigurer {
 
     @Value("${env.client-base-url}")
